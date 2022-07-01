ad3d2d7145350e841b0816d42a7d83d5d3e07c08
sinkaroid <bibsindra@gmail.com>
2022-07-01 15:54:19 +0700

    cum

--- thread.py
+++ thread.py
@@ -10,7 +10,7 @@ from utils.requests import better_object, deserialize, fetch, hack
 enpoint = ["fgo", "genshin_impact", "azur_lane", "waifu", "arknights", 
 "fire_emblem", "gfl", "hololive", "kancolle", "sex", "gelbooru", "r34", "safebooru"]
 
-animesex = ["ass", "bdsm"]
+animesex = ["ass", "bdsm", "cum"]
 
 def setInterval(timer, task):
     isStop = task()
