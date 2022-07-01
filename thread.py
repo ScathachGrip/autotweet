a3db1fbce6e78a6f5125136279f2dff7cb343920
sinkaroid <bibsindra@gmail.com>
2022-07-01 16:08:37 +0700

    thighs

--- thread.py
+++ thread.py
@@ -12,7 +12,7 @@ enpoint = ["fgo", "genshin_impact", "azur_lane", "waifu", "arknights",
 
 animesex = ["ass", "bdsm", "cum", "creampie", "manga", "femdom", "hentai", "incest",
             "masturbation", "public", "ero", "orgy", "elves", "yuri", "pantsu",
-            "glasses", "cuckold", "blowjob", "boobjob", "foot"]
+            "glasses", "cuckold", "blowjob", "boobjob", "foot", "thighs"]
 
 
 def setInterval(timer, task):
